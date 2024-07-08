ba21458746a352e1c4a1f1f28fffb1f75373da27
habaekk <haback1493@gmail.com>
2024-07-08 23:14:10 +0900

    edit content for user page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,36 +1,22 @@
-'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
-import Header from "../component/Header"
+import Header from "../component/Header";
 
 export default function UserPage() {
   return (
     <div className="main-content flex min-h-screen flex-col items-center justify-center p-24 relative">
       <Header />
 
-      {/* 상단 로고 */}
-      <div className="mb-8 mt-16">
-        <Link href="/" className="cursor-pointer">
-          <Image
-            src="/logo.png" // 로고 이미지 파일 경로
-            alt="Logo"
-            width={100}
-            height={50}
-          />
-        </Link>
-      </div>
-
       {/* 이미지와 텍스트 섹션 */}
       <div className="flex items-center mb-8">
         <Image
-          src="/profile.png" // 이미지 파일 경로
-          alt="Profile Image"
+          src="/WLD.png" // 이미지 파일 경로
+          alt="WLD Logo"
           width={50}
           height={50}
-          className="mr-4"
+          className="mr-2"
         />
-        <p className="text-lg">텍스트 내용</p>
+        <p className="text-2xl font-bold">월드코인 앱을 이용하여 로그인하세요</p>
       </div>
 
       {/* 중앙 QR 코드 영역 */}
@@ -40,17 +26,15 @@ export default function UserPage() {
 
       {/* QR 코드 아래의 텍스트 */}
       <div className="mb-8">
-        <p className="text-lg">QR 코드 아래의 텍스트</p>
+        <p className="text-m text-gray-500">또는 코드를 이용하여 로그인</p>
       </div>
 
-      {/* 하단 숫자 코드를 입력할 네모 박스 */}
+      {/* 하단 숫자 코드를 표시하는 네모 박스 */}
       <div className="text-center">
-        <input 
-          type="text"
-          className="border-2 border-gray-400 px-4 py-2 rounded mb-4"
-          placeholder="코드 텍스트"
-        />
-        <p className="text-sm">Additional text or instructions here</p>
+        <div className="border-2 border-gray-400 px-4 py-2 rounded mb-4">
+          <p className="text-lg">1234-5678-9101</p>
+        </div>
+        <p className="text-sm"></p>
       </div>
     </div>
   );
