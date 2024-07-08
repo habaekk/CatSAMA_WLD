33344c0d47c27e4bd2c2792d0f23fec9aa687613
habaekk <haback1493@gmail.com>
2024-07-08 22:47:56 +0900

    edit header logo for link to home page

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -6,14 +6,14 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="w-full flex justify-between items-center p-4 absolute top-0 left-0">
-      <div className="top-4 left-4">
+      <Link href="/" className="top-4 left-4 cursor-pointer">
         <Image
           src="/logo.webp" // 로고 이미지 파일 경로
           alt="Logo"
           width={40}
           height={40}
         />
-      </div>
+      </Link>
       <Link href="/user" className="top-4 right-4 cursor-pointer">
         <Image
           src="/user.png" // 사용자 아이콘 이미지 파일 경로
