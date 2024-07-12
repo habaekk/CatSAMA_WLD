5fa46c8a4bd3568bc85c00e5905a5a05857a627b
habaekk <haback1493@gmail.com>
2024-07-12 19:24:37 +0900

    add world ID login button

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,20 +1,22 @@
+'use client';
+
 import Image from 'next/image';
 import Header from "../component/Header";
 import Footer from "../component/Footer";
 import { IDKitWidget, VerificationLevel } from '@worldcoin/idkit';
 
+function onSuccess(result) {
+  // 인증 성공 시 실행되는 코드
+  console.log('Verification successful:', result);
+  window.alert(`Successfully verified with World ID! Your nullifier hash is: ` + result.nullifier_hash);
+}
+
+function handleVerify(result) {
+  // 증명이 수신될 때 실행되는 선택적 콜백
+  console.log('Proof received:', result);
+}
+
 export default function UserPage() {
-  const onSuccess = (result) => {
-    // 인증 성공 시 실행되는 코드
-    console.log('Verification successful:', result);
-    window.alert(`Successfully verified with World ID! Your nullifier hash is: ` + result.nullifier_hash);
-  };
-
-  const handleVerify = (result) => {
-    // 증명이 수신될 때 실행되는 선택적 콜백
-    console.log('Proof received:', result);
-  };
-
   return (
     <div className="main-content flex min-h-screen flex-col items-center justify-center p-24 relative">
       <Header />
@@ -31,19 +33,23 @@ export default function UserPage() {
         <p className="text-2xl font-bold">월드코인 앱을 이용하여 로그인하세요</p>
       </div>
 
+      <IDKitWidget
+        app_id="app_GBkZ1KlVUdFTjeMXKlVUdFT" // Developer Portal에서 얻은 app_id
+        action="vote_1" // Developer Portal에서 얻은 action id
+        onSuccess={onSuccess} // 인증 성공 시 실행되는 콜백 함수
+        handleVerify={handleVerify} // 증명 수신 시 실행되는 선택적 콜백 함수
+        verification_level={VerificationLevel.Device}
+      >
+        {({ open }) => (
+          <button 
+            onClick={open} 
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          >
+            World ID 로 로그인 
+          </button>
+        )}
+      </IDKitWidget>
 
-      {/* QR 코드 아래의 텍스트 */}
-      <div className="mb-8">
-        <p className="text-m text-gray-500">또는 코드를 이용하여 로그인</p>
-      </div>
-
-      {/* 하단 숫자 코드를 표시하는 네모 박스 */}
-      <div className="text-center">
-        <div className="border-2 border-gray-400 px-4 py-2 rounded mb-4">
-          <p className="text-lg">1234-5678-9101</p>
-        </div>
-        <p className="text-sm"></p>
-      </div>
 
       <Footer />
     </div>
