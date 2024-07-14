72230d67c5336fe8a652941fc338af6df68aa20e
habaekk <haback1493@gmail.com>
2024-07-14 16:40:06 +0900

    edit settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,14 +1,20 @@
 // pages/settings/page.tsx
 
 import React from 'react';
-import SettingsLayout from './layout';
+import Header from '../component/Header';
+import Footer from '../component/Footer';
 
 const SettingsPage: React.FC = () => {
   return (
-    <SettingsLayout>
-      <h2>Welcome to the Settings Page</h2>
-      <p>Here you can customize your settings.</p>
-    </SettingsLayout>
+    <div className="main-content flex min-h-screen flex-col items-center justify-center p-24 relative">
+        <Header />
+        <div>
+            <h2>Welcome to the Settings Page</h2>
+            <p>Here you can customize your settings.</p>
+        </div>
+        <Footer />
+        
+    </div>
   );
 };
 
