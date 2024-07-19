78b7f16363274841ead40956b71cb6ff28aa4c82
habaekk <haback1493@gmail.com>
2024-07-19 19:50:57 +0900

    FIX: fix flickering of dark mode button when refresh. add loading button

--- app/component/CategoryCard.tsx
+++ app/component/CategoryCard.tsx
@@ -8,7 +8,7 @@ interface CategoryCardProps {
 const CategoryCard: React.FC<CategoryCardProps> = ({ title, link }) => {
   return (
     <Link href={link}>
-      <div className="w-80 h-40 bg-blue-500 text-white rounded-lg flex items-start justify-start text-3xl font-bold m-4 transition-transform transform hover:scale-105 dark:bg-blue-700 cursor-pointer p-4">
+      <div className="w-80 h-40 bg-blue-500 text-white rounded-lg flex items-start justify-start text-3xl font-bold m-2 transition-transform transform hover:scale-105 dark:bg-blue-700 cursor-pointer p-4">
         {title}
       </div>
     </Link>
--- app/component/DarkModeToggle.tsx
+++ app/component/DarkModeToggle.tsx
@@ -1,9 +1,17 @@
 import { useRecoilState } from 'recoil';
 import { darkModeState } from '../state/theme';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const DarkModeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useRecoilState(darkModeState);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const savedDarkMode = localStorage.getItem('darkMode') === 'true';
+    setIsDarkMode(savedDarkMode);
+    document.documentElement.classList.toggle('dark', savedDarkMode);
+    setIsMounted(true);
+  }, [setIsDarkMode]);
 
   const toggleDarkMode = () => {
     const newDarkModeState = !isDarkMode;
@@ -12,12 +20,15 @@ const DarkModeToggle = () => {
     document.documentElement.classList.toggle('dark', newDarkModeState);
   };
 
+  if (!isMounted) {
+    return <button className="mt-4 px-4 py-2 bg-gray-200 text-gray-800 rounded" disabled>Loading...</button>;
+  }
+
   return (
     <button onClick={toggleDarkMode}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded dark:bg-yellow-500"
     >
       {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-
     </button>
   );
 };
