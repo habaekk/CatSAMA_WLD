116c65e29a3ef2e9383e4de2b3de73d0ec0cb348
habaekk <haback1493@gmail.com>
2024-07-20 16:42:29 +0900

    REFACTOR: move LLMSerive part from ChatWindow

--- app/components/Chat/ChatWindow.tsx
+++ app/components/Chat/ChatWindow.tsx
@@ -3,74 +3,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import ChatMessage from './ChatMessage';
 import ChatInput from './ChatInput';
-
-interface Message {
-  role: 'assistant' | 'user' | 'system';
-  content: string;
-}
-
-const initialMessages: Message[] = [
-  {
-    role: 'system',
-    content: `
-    You are a cat assistant called catSAMA.
-
-  Use emoji to be cute. Use grammatically correct words.
-
-  You are a part of home IOT system with Home Assistant
-
-  You must distinguish which user want to make a casual chat or control&query of home devices.
-
-  In case of casual chat {
-  you can freely chat with human user.
-  }
-  In case of Control & Query of home device {
-  Just response with 'THIS IS IOT SERVICE'
-  DO NOT response any other things.
-  }
-    `,
-  },
-];
+import { chat, initialMessages, Message } from './LLMService';
 
 const ChatWindow: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>(initialMessages);
   const [loading, setLoading] = useState(false);
   const chatWindowRef = useRef<HTMLDivElement>(null);
-
-  const chat = async (messages: Message[]): Promise<Message> => {
-    const body = {
-      model: 'Ccat',
-      messages: messages,
-    };
-
-    const response = await fetch('http://localhost:11434/api/chat', {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const reader = response.body?.getReader();
-    if (!reader) {
-      throw new Error('Failed to read response body');
-    }
-
-    let content = '';
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
-        break;
-      }
-      const rawjson = new TextDecoder().decode(value);
-      const json = JSON.parse(rawjson);
-
-      if (json.done === false) {
-        content += json.message.content;
-      }
-    }
-    return { role: 'assistant', content: content };
-  };
 
   const handleSendMessage = async (messageContent: string) => {
     const userMessage: Message = { role: 'user', content: messageContent };
