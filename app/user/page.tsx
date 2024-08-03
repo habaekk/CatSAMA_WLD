b732485bd330c6e6a4c2ec867beb5261536e0b6f
habaekk <haback1493@gmail.com>
2024-08-03 14:52:27 +0900

    rm password field on login page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -36,43 +36,8 @@ export default function UserPage() {
         {/* 큰 글씨로 CatSAMA */}
         <h1 className="text-5xl font-extrabold mb-8">CatSAMA</h1>
 
-        {/* 이메일 입력 필드 */}
-        <input
-          type="email"
-          placeholder="이메일"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full max-w-md p-4 mb-4 text-black dark:text-white bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded"
-        />
-
-        {/* 비밀번호 입력 필드 */}
-        <input
-          type="password"
-          placeholder="비밀번호"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full max-w-md p-4 mb-4 text-black dark:text-white bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded"
-        />
-
-        {/* 로그인 버튼 */}
-        <button onClick={handleLogin} className="w-full max-w-md bg-blue-500 text-white p-4 rounded hover:bg-blue-600 mb-4">
-          로그인
-        </button>
-
-        {/* 아이디가 없으세요? 가입하기 */}
-        <div className="mb-6">
-          아이디가 없으세요? <a href="/signup" className="text-blue-500 hover:underline mb-6">가입하기</a>
-        </div>
-
-        {/* 디바이드 줄 */}
-        <div className="flex items-center w-full max-w-md my-4">
-          <hr className="w-full border-gray-300 dark:border-gray-700" />
-          <span className="mx-4 text-gray-500 dark:text-gray-400 whitespace-nowrap">또는</span>
-          <hr className="w-full border-gray-300 dark:border-gray-700" />
-        </div>
-
         {/* 월드코인으로 로그인 섹션 */}
-        <div className="flex flex-col items-center">
+        <div className="w-full max-w-md flex flex-col items-center mb-4">
           <IDKitWidget
             app_id={process.env.NEXT_PUBLIC_APP_ID} // 환경 변수에서 가져온 app_id
             action={process.env.NEXT_PUBLIC_ACTION_ID} // 환경 변수에서 가져온 action id
@@ -83,9 +48,9 @@ export default function UserPage() {
             {({ open }) => (
               <button 
                 onClick={open} 
-                className="bg-blue-500 text-white px-4 py-3 rounded-lg hover:bg-blue-600 transition-colors duration-300"
+                className="w-full bg-blue-500 text-white px-4 py-3 rounded-lg hover:bg-blue-600 transition-colors duration-300"
               >
-                <div className="flex items-center">
+                <div className="flex items-center justify-center">
                   <Image
                     src="/WLD.svg" // 이미지 파일 경로
                     alt="WLD Logo"
@@ -93,12 +58,33 @@ export default function UserPage() {
                     height={30}
                     className="mr-2 mb-1"
                   />
-                  <p className="text-lg font-bold mr-4">월드코인 앱을 이용하여 로그인</p>
+                  <p className="text-lg font-bold">월드코인 앱을 이용하여 로그인</p>
                 </div>
               </button>
             )}
           </IDKitWidget>
         </div>
+
+        {/* 디바이드 줄 */}
+        <div className="flex items-center w-full max-w-md my-4">
+          <hr className="w-full border-gray-300 dark:border-gray-700" />
+          <span className="mx-4 text-gray-500 dark:text-gray-400 whitespace-nowrap">또는</span>
+          <hr className="w-full border-gray-300 dark:border-gray-700" />
+        </div>
+
+        {/* 이메일 입력 필드 */}
+        <input
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full max-w-md p-4 mb-4 text-black dark:text-white bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded"
+        />
+
+        {/* 로그인 버튼 */}
+        <button onClick={handleLogin} className="w-full max-w-md bg-blue-500 text-white p-4 rounded hover:bg-blue-600 mb-4">
+          로그인
+        </button>
       </div>
     </div>
   );
