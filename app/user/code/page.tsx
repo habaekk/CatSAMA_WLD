c0f1762cfb50749f1fa181d68387796ad4dee78e
habaekk <haback1493@gmail.com>
2024-08-06 22:55:32 +0900

    UIUIX: add button hover effect, adjust mb, adjust focus on input filed

--- app/user/code/page.tsx
+++ app/user/code/page.tsx
@@ -43,18 +43,18 @@ export default function Code() {
         <p className="mb-4 text-m">이메일로 보내진 6자리 코드를 입력해주세요.</p>
 
         {/* 인증 코드 입력 필드 및 제출 버튼 */}
-        <div className="w-full max-w-md flex items-center mb-2">
+        <div className="w-full max-w-md flex items-center mb-4">
           <input
             type="text"
             placeholder="인증 코드*"
             value={verificationCode}
             onChange={(e) => setVerificationCode(e.target.value)}
             onKeyPress={handleKeyPress}
-            className={`flex-grow p-4 text-black dark:text-white bg-white dark:bg-gray-800 border ${codeError ? 'border-red-500' : 'border-gray-300 dark:border-gray-700'} rounded-md`}
+            className={`flex-grow p-4 text-black dark:text-white bg-white dark:bg-gray-800 border ${codeError ? 'border-red-500' : 'border-gray-300 dark:border-gray-700'} rounded-md focus:outline-none focus:ring-2 ${codeError ? 'focus:ring-red-500' : 'focus:ring-blue-500'}`}
           />
           <button 
             onClick={handleCodeSubmit} 
-            className="bg-blue-500 text-white p-4 rounded-md hover:bg-blue-600 flex items-center justify-center ml-2"
+            className="bg-blue-500 text-white p-4 rounded-md hover:bg-blue-600 flex items-center justify-center ml-2 transition-colors duration-200"
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
               <line x1="5" y1="12" x2="19" y2="12"></line>
